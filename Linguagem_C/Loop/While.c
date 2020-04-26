b6a08275d45273fc8c8ed233c610ad3d4eb9abd7
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-26 20:51:30 -0300

    Create While.c

---
+++ Linguagem_C/Loop/While.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * 
+ * Maurício Freire
+ * Laço while.
+ * While loop.
+*/
+
+int main(void) {
+    int i = 10;;
+
+    printf("Contagem infinita: \n");
+    while (i > 0) {
+        printf("%d\n", i--);
+    }
+    return 0;
+}
+
