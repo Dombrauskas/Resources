c5c3b3f2161b58d8bdd30d31a7a9ecded9317980
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-26 21:30:35 -0300

    Create RenameImport.py

---
+++ Python/Biblioteca/RenameImport.py
@@ -0,0 +1,12 @@
+"""
+ " 
+ " Maurício Freire
+ " É possível renomear bibliotecas importadas.
+ " It is possible to rename imported libraries.
+"""
+
+from math import sqrt as raiz
+
+x = int(input("Informe um número: "))
+print(raiz(x))
+
