e42160ac31e1ed8d72b9507ebc11cf3ca1b22c29
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-27 15:57:20 -0300

    Add files via upload

---
+++ Java/Applet/Esqueleto.java
@@ -0,0 +1,36 @@
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * @author Maurício Freire
+ * Date 17/04/2020 at 14:20
+ * Created on IntelliJ IDEA
+ * <applet code="Esqueleto" width=140 height=140></applet>
+ */
+public class Esqueleto extends Applet {
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
+
