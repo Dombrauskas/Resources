c4f7aeacf5dd10fb2196bc907749ff62b1664ad3
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-28 15:34:57 -0300

    Create Null.c

---
+++ Linguagem_C/Ponteriro/Null.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * 
+ * Maurício Freire
+ * Se não tiver um valor para o ponteiro atribua-o NULL.
+ * If there is no value to the pointer assign NULL to it.
+*/
+
+int main()
+{
+    int *pointer = NULL;
+
+    printf("Valor de pointer: %d\n", pointer);
+    return 0;
+}
+
