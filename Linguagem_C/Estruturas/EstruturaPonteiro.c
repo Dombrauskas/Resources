e6c37addee52e9e174bd2c17b1f2dc0f61f9851b
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-05-10 17:07:17 -0300

    Create EstruturaPonteiro.c

---
+++ Linguagem_C/Estruturas/EstruturaPonteiro.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * 
+ * Maurício Freire
+ * Membros de uma estrutura podem ser acessados de duas formas ou por ponto
+ * (.), se for acessado por uma variável do tipo da estrutura, ou por seta
+ * (->) se for acessado por um ponteiro.
+ * Members of a struct can be acessed by two different operator the dot (.)
+ * whether it is accessed by a struct type variable, or by the arrow (->)
+ * whether it is accessed by a pointer.
+*/
+
+
+typedef struct {
+    char rua[30];
+    int num;
+    char bairro[15];
+} Home;
+
+int main() 
+{
+    Home home, address, *phome;
+
+    printf("Rua: ");
+    scanf(" %29[^\n]", home.rua);
+    printf("Num: ");
+    scanf(" %d", &home.num);
+    printf("Bairro: ");
+    scanf(" %14[^\n]", home.bairro);
+    phome = &home;
+
+    printf("\n");
+
+    printf("Rua: ");
+    scanf(" %29[^\n]", address.rua);
+    printf("Num: ");
+    scanf(" %d", &address.num);
+    printf("Bairro: ");
+    scanf(" %14[^\n]", address.bairro);
+
+    printf("Ponteiro mora em %s %d, %s\n", phome->rua, phome->num, phome->bairro);
+    printf("Voce mora em %s %d, %s\n", home.rua, home.num, home.bairro);
+    return 0;
+}
+
