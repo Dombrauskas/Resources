45e6fe1b1553af0b3454f4283ddbb5cc481c54fb
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-05-20 22:29:55 -0300

    Create Exemplo2.kt

---
+++ Kotlin/Variáveis/Exemplo2.kt
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Maurício Freire
+ * Apesar de não ser preciso declarar o tipo da variável, pois Kotlin faz isso
+ * implicitamente, é possível especificar o tipo delas.
+ * Besides it is not needed to declare the variable type, since Kotlin does it
+ * implicitly, it is possible to specify its type.
+ */
+
+fun main(args : Array<String>) {
+    var x: Int = 10
+    val y: String = "Kotlin"
+    
+    println(x)
+    println(y)
+}
+
+
