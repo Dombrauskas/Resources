00bc1fea0ea7698013cbe412023b619cdb4387c8
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-09-26 16:28:22 -0300

    Add files via upload

---
+++ Java/IO/OutputStream_write.java
@@ -0,0 +1,16 @@
+package Pgm;
+
+/**
+ *
+ * @author Maurício Freire
+ * Date 26/09/2020 at 15:47
+ * Created on IntelliJ IDEA
+ */
+
+public class OutputStream_write {
+    public static void main(String[] args) {
+        int b = 'J';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
