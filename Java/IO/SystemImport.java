ff12b77f24a7ebcd415706c8e9785f80c6a5980d
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2021-01-18 21:32:23 -0300

    Create SystemImport.java

---
+++ Java/IO/SystemImport.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+
+public class SystemImport {
+    public static void main(String[] args) {
+        out.println("Exibir mensagem na tela sem chamar o System");
+    }
+}
+
